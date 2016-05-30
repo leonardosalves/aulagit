42b9573ffc77042f8debaa5cf7bb2cb7c4957dc6
Suporte TI - Restauração <Suporte TI - Restaurao>
2016-05-30 20:03:57 -0300

    Lançando uma nova funcionalidade. Incrementando o minor version!

--- aula1.php
+++ aula1.php
@@ -5,3 +5,4 @@ Esta linha foi adicionada no branch MASTER!!!!! E agora, quem vai prevalecer?
 
 Esta linha foi adicionada dentro do branch funcao!
 
+Agora sim uma fuincionalidade que vai revolucionar este projeto.
